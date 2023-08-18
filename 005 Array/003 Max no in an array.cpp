c040b3d2d893db4c1af8e28c42c23e009f5d011a
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-08-18 18:32:18 +0530

    max no

--- 005 Array/003 Max no in an array.cpp
+++ 005 Array/003 Max no in an array.cpp
@@ -3,7 +3,17 @@
 using namespace std;
 int main(){
     int arr[] = {1,2,23,44,54,21,2,567,6,-1};
+    int n = 10;
     int maxi = INT_MIN;
     int mini = INT_MAX;
     
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > maxi)
+        {
+                maxi = arr[i];
+        }
+        
+    }
+    cout<<"Max no. is : "<<maxi<<endl;
 }
