d3d476bd2f4a0de801e1c6031b867f137a908698
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-08-23 21:50:25 +0530

    sort 0 & 1, move -ve one side

---
+++ 006 Vector/Problems.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <limits.h>
+using namespace std;
+int main()
+{
+
+    // Prob 1 Sort 0, 1 & 2 ----------------------------------------------------------------
+    //     vector<int>arr{1,0,1,0,1,1,1,0,0,0,1,1,};
+    //     int start = 0;
+    //     int end = arr.size() - 1;
+    //     int ind = 0;
+
+    // while (ind<end)
+    // {
+    //     if (arr[ind]==0)
+    //         {
+    //             start++;
+    //             ind++;
+    //         }
+    //         else{
+    //             swap(arr[start],arr[end]);
+    //             end--;
+    //         }
+    // }
+
+    //     //Printing arr ---------
+
+    //     for (int i = 0; i < arr.size(); i++)
+    //     {
+    //         cout<<arr[i]<<" ";
+    //     }
+
+    // Prob 2 Move -ve no to one side -----------------------------------------------------------
+
+    vector<int> arr{-1, 1, 2, -2, 3, -3, 4, -4, 5, -5, 6, -6, 7, -7, 8, -8, 9, -9};
+    int start = 0;
+    int end = arr.size() - 1;
+    int ind = 0;
+
+    while (end>start)
+    {
+        if (arr[ind] < 0)
+        {
+            start++;
+            ind++;
+        }
+        else
+        {
+            swap(arr[start], arr[end]);
+            end--;
+        }
+    }
+
+    // Printing arr ---------
+
+    for (auto v : arr)
+    {
+        cout << v << " ";
+    }
+
+    // Prob 3 find duplicate element ---------------------------------------------------------
+
+    // vector<int>arr{1,2,3,1,9,4,8,6,3,1,4,7,7,3};
+}
