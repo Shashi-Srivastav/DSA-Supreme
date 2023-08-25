a33bb20929a5726fa7df2b6545db585d4f72252b
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-08-25 21:17:14 +0530

    find missing element

--- 005 Array/005 Problems.cpp
+++ 005 Array/005 Problems.cpp
@@ -1,6 +1,29 @@
 #include<iostream>
 using namespace std;
+
+void findMissing(int *a, int n){
+    //visited arr
+    for (int i = 0; i < n; i++)
+    {
+        int index = abs(a[i]);
+        if (a[index - 1]>0)
+        {
+            a[index -1] *= -1;
+        }
+        
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    
+    
+}
+
 int main(){
     int n;
     int a[]={1,3,5,3,4};
+    n = sizeof(a)/sizeof(int);
+    findMissing(a,n);
+    return 0;
 }
