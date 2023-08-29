61b513ef266564526374eddd92b76d0968a3a2ee
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-08-29 22:57:08 +0530

    sqrt fx

--- 008 Searching & Sorting/Problems.cpp
+++ 008 Searching & Sorting/Problems.cpp
@@ -55,17 +55,51 @@ int lastOcc(vector<int>arr,int key){
     
 }
 
+int findSqrt(int n){
+    int s = 0;
+    int e = n;//not an array
+    int mid = s+(e-s)/2;
+    int ans = -1;
+    while (s<=e)
+    {
+        if (mid*mid == n)
+        {
+            ans = mid;
+            return ans;
+        }
+        else if(mid*mid > n){
+            //left search
+            e = mid -1;
+        }
+        else{
+            //storing ans that is nearest to result
+            ans = mid;
+            //right search
+            s = mid+1;
+        }
+        mid = s+(e-s)/2;
+    }
+    return ans;
+}
+
 
 int main()
 {
 
     // Prob Find First Occurance of Repated Element --------------------------------------------------------------------
 
-    vector<int> arr{1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 6, 7, 8, 9};
-    int key = 4;
-    // int firstOcc = findFirstOcc(arr,key);
-    // cout << "First occ at " << firstOcc << endl;
+    // vector<int> arr{1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 6, 7, 8, 9};
+    // int key = 4;
+    // // int firstOcc = findFirstOcc(arr,key);
+    // // cout << "First occ at " << firstOcc << endl;
 
-    int lastOc = lastOcc(arr,key);
-    cout<<"Last Occ : "<<lastOc<<endl;
+    // int lastOc = lastOcc(arr,key);
+    // cout<<"Last Occ : "<<lastOc<<endl;
+
+    //Prob  Find Sqrt of no. ---------------------------------------------------------------------------------------------
+
+    int n;
+    cin>>n;
+    int ans  = findSqrt(n);
+    cout<<"Sqrt of "<<n<<" is "<<ans;
 }
