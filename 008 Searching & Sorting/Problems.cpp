7d65128cd2bdfcefd21f1e91bb84c7d0d77be9b3
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-08-31 19:58:58 +0530

    del

--- 008 Searching & Sorting/Problems.cpp
+++ 008 Searching & Sorting/Problems.cpp
@@ -107,6 +107,32 @@ bool binarySearchin2DArr(int arr[][5], int row, int col, int target){
     return false;
 }
 
+int findPivortElement(vector<int>arr){
+    int s = 0;
+    int e = arr.size()-1;
+    int mid = s+(e-s)/2;
+    while (s<=e)
+    {
+        if(s == e ){
+            return s;
+        }
+        if (mid <= e && arr[mid] > arr[mid + 1])
+            return mid;
+        if(mid - 1 >= s && arr[mid - 1] > arr[mid])
+            return mid - 1;
+        if(arr[s]>arr[mid]){
+            e = mid - 1;
+        }
+        else{
+            s = mid+1;
+        }
+        mid = s + (e-s)/2;
+        
+    }
+    return -1;
+
+}
+
 int main()
 {
 
@@ -127,21 +153,27 @@ int main()
 //     int ans  = findSqrt(n);
 //     cout<<"Sqrt of "<<n<<" is "<<ans;
 
-//     Find element index of 2d arr using binary search -------------------------------------------------------------------
-    int arr[4][5] = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20}};
-    int row = 4;
-    int col = 5;
-    int target = 19;
-    bool ans = binarySearchin2DArr(arr,row,col,target);
+//    Find element index of 2d arr using binary search -------------------------------------------------------------------
+    // int arr[4][5] = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20}};
+    // int row = 4;
+    // int col = 5;
+    // int target = 19;
+    // bool ans = binarySearchin2DArr(arr,row,col,target);
 
-    if (ans)
-    {
-        cout<<"Found";
-    }
-    else{
-        cout<<"Not Found";
-    }
+    // if (ans)
+    // {
+    //     cout<<"Found";
+    // }
+    // else{
+    //     cout<<"Not Found";
+    // }
         
-    
+    //Find Pivot Element in Roated Sorted Array with no. Duplicate Element---------------------------------------------------
+
+    //Pivot = Max element in ann Array
+    vector<int>arr{2,3,4,5,6,7,8,9,10,1};
+    int ans = findPivortElement(arr);
+    cout<<"Pivot element foind ar index "<<ans;
+
 
 }
