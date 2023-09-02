a0743c2974b2f5b737701096c40a5c3bde170b6b
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-09-02 19:36:55 +0530

    expo search / search space prob

--- 008 Searching & Sorting/Problems.cpp
+++ 008 Searching & Sorting/Problems.cpp
@@ -133,6 +133,31 @@ int findPivortElement(vector<int>arr){
 
 }
 
+    int binSearch(int arr[], int s, int e, int x){
+        while(s<=e){
+            int mid = (s+e)/2;
+            if(arr[mid] == x){
+                return mid;
+            }
+            else if(x>arr[mid]){
+                s = mid+1;
+            }
+            else{
+                e = mid-1;
+            }
+        }
+        return -1;
+    }
+
+    int expoSearch(int arr[], int n , int x){
+        if(arr[0] == x) return 0;
+        int i = 1;
+        while(i<n && arr[i] <= x){
+            i = i*2;
+        }
+        return binSearch(arr,i/2,min(i,n-1),x);
+    }
+
 int main()
 {
 
@@ -181,9 +206,10 @@ int main()
     //Strazilc Search
 
     //Only Apply on Sorted Array --------
-
-    
-
-
-
+    int arr[] = {3,4,5,6,11,13,14,15,56,75};
+    int n = sizeof(arr)/sizeof(int);
+    int x = 75;
+    int ans = expoSearch(arr,n,x);
+    cout<<ans<<endl;
+    return 0;
 }
