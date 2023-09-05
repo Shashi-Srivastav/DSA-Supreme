38813a4ce201a8359a6417cf980a135d18bf1eb7
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-09-05 22:05:42 +0530

    char arr decleration / input a string

---
+++ 009 Char Array & String/Char Array/intro.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+using namespace std;
+
+int main(){
+    // char name[100];
+    // name[0] = 's';
+    // name[1] = 'h';
+    // name[2] = 'a';
+    // name[3] = 's';
+    // name[4] = 'h';
+    // name[5] = 'i';
+    // cout<<"Your name is "<<name<<endl;
+
+    //mthd 2 ----------------
+
+    // cin>>name;
+    // cout<<"Your name is "<<name<<endl;
+
+    //There is always Null Char at the end of string in array - Eg
+
+    // cin>>name;
+    // for (int i = 0; i < 7; i++)
+    // {
+    //     cout<<"index "<<i<<" value is "<<name[i]<<endl;
+    // }
+    // index 0 value is s
+    // index 1 value is h
+    // index 2 value is a
+    // index 3 value is s
+    // index 4 value is h
+    // index 5 value is i
+    // index 6 value is
+
+    //Null Char act as terminator for string in an array
+
+    // int value = (int)name[6];
+    // cout<<"index value of 6 as int is "<<value<<endl;
+    //ASCII value of 0 = \0 Null Char
+
+
+}
