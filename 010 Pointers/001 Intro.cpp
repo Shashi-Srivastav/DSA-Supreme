568c03e62414a22e2e793877210efe000dfeb302
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-09-15 12:16:45 +0530

    Pointers

--- 010 Pointers/001 Intro.cpp
+++ 010 Pointers/001 Intro.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 using namespace std;
+    void solve(int arr[]){
+        cout<<"Size of arr inside solve fx "<<sizeof(arr)<<endl;
+    }
+    void update(int *p){
+        cout<< "address stored inside p is : "<<p<<endl;//address stored inside p is : 0x62febc
+        cout<<"Address of p is : "<<&p<<endl; //Address of p is : 0x62fea0
+    }
 int main()
 {
     // int a = 5;
@@ -10,7 +17,7 @@ int main()
     // cout<<&b<<endl;//a, b address are diff
 
     // pointer cration
-    //  int* ptr = &a;
+    //  int* ptr = &a;// *ptr = a - error
     // accessing value using derefrence
     //  cout<<*ptr<<endl;//dereferance - output - 5
     //  cout<<ptr<<endl;//gives loc
@@ -60,7 +67,7 @@ int main()
     // cout<< (*p/2) - (*q)/2 <<endl;//0
 
     // Class 2 --------------------------------------------------------------------------------------------
-    int arr[4] = {12, 33, 44, 55};
+    // int arr[4] = {12, 33, 44, 55};
 
     // cout<<arr<<endl;//0x62feb0
     // cout<<arr[0]<<endl;//12
@@ -71,14 +78,72 @@ int main()
     // cout<<p<<endl;//0x62feb0 - diff
     // cout<<&p<<endl;//0x62feac - diff
 
-    cout << *arr << endl;
-    cout << arr[0] << endl;
-    cout << *arr + 1 << endl;
-    cout << *(arr) + 1 << endl;
-    cout << *(arr + 1) << endl;
-    cout << arr[1] << endl;
-    cout << *(arr + 2) << endl;
-    cout << arr[2] << endl;
-    cout << *(arr + 3) << endl;
-    cout << arr[3] << endl;
+    // cout << *arr << endl;//12
+    // cout << arr[0] << endl;//12
+    // cout << *arr + 1 << endl;//13
+    // cout << *(arr) + 1 << endl;//13
+    // cout << *(arr + 1) << endl;//33
+    // cout << arr[1] << endl;//33
+    // cout << *(arr + 2) << endl;//44
+    // cout << arr[2] << endl;//44
+    // cout << *(arr + 3) << endl;//55
+    // cout << arr[3] << endl;//55
+
+    // int i = 0;
+    // cout<<arr[i]<<endl;//12
+    // cout<<i[arr]<<endl;//12
+    // cout<<*(arr+i)<<endl;//12
+    // cout<<*(i+arr)<<endl;//12 
+
+    // int arr[10];
+    // cout<<sizeof(arr)<<endl;// 4bytes*10 = 40
+
+    // int*ptr = arr;
+    // cout<<sizeof(ptr)<<endl;//4/8 system dep
+    // cout<<sizeof(*ptr)<<endl;//4
+
+    // char ch[10] = "Shashi";
+    // char*c = ch;
+    // cout<<ch<<endl;//shashi
+    // cout<<&ch<<endl;//0x62fe23
+    // cout<<ch[0]<<endl;//s
+    // cout<<&c<<endl;//0x62feb0
+    // cout<<*c<<endl;//s
+    // cout<<c<<endl;//shashi
+
+    // char name[9] = "ShErBano";
+    // char* c = &name[0];
+    // cout<<name<<endl;//ShErBano
+    // cout<<&name<<endl;//0x62feb7
+    // cout<<*(name+3)<<endl;//r
+    // cout<<c<<endl;//ShErBano
+    // cout<<&c<<endl;//0x62feb0
+    // cout<<*(c+3)<<endl;//r
+    // cout<<c+2<<endl;//ErBano
+    // cout<<*c<<endl;//s
+    // cout<<c+8<<endl;//\0
+
+    // char ch = 'k';
+    // char* cptr = &ch;
+    // cout<<cptr<<endl;//printing till found \0 - k╗■b
+
+    // char name[10] = "Shashi";
+    //cout<<name<<endl;//Shashi - good practice as temp memo to main mem by copy
+
+    // char*c = "Shashi";
+    //cout<<c<<endl;//Shashi - bad practice as temp stored and *c pointing to temp string may loss data
+
+    //Pointer with function ---------------------------------------------------------
+    // int arr[10] = {1,2,3,4};
+    // cout<<"Size of arr inside main fx "<<sizeof(arr)<<endl;//40
+    // solve(arr);//4 @pass by ref as a pointer not array
+
+    // int a = 5;
+    // cout<<"Addrss af a is "<<&a<<endl;//Addrss af a is 0x62febc
+    // int*ptr = &a;
+    // cout<<"Address stored in ptr is : "<<ptr<<endl;//Address stored in ptr is : 0x62febc 
+    // cout<<"Address of ptr is : "<<&ptr<<endl;//Address of ptr is : 0x62feb8
+    // update(ptr);
+    // cout<<"value of a is : "<<a<<endl;//value of a is : 5
+    return 0;
 }
