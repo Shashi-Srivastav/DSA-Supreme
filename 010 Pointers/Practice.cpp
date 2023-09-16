db1710a6a41bf7d294bd8aebf9afda5c29c8379c
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-09-16 10:58:52 +0530

    sieve optimisation, ptr prob

---
+++ 010 Pointers/Practice.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // float f = 10.5;
+    // float p = 2.5;
+    // float* ptr = &f;
+    // (*ptr)++;
+    // *ptr = p;
+    // cout<<*ptr<<" "<<f<<" "<<p<<endl;
+
+    // int a = 7, b = 17, *c = &b;
+    // *c = 7;
+    // cout<<a<<" "<<b<<endl; 
+
+    // int *ptr = 0, a = 10;
+    // *ptr = a;
+    // cout<<*ptr<<endl;//error @null pointer
+
+    
+}
