ad7a793a9e985eb2a6fe13f6f773009b907ceb30
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-10-21 17:07:55 +0530

    backtracking

---
+++ 013 DnC & Backtracking/class 2 QuickSort V2.cpp
@@ -0,0 +1,28 @@
+
+// public:
+//     //Function to sort an array using quick sort algorithm.
+//     void quickSort(int a[], int start, int end)
+//     {
+//         if(start >= end) return;
+//         int pivot = end;
+//         int i = start -1;
+//         int j = start;
+//         while(j<pivot){
+//             if(a[j] < a[pivot]){
+//                 ++i;
+//                 swap(a[i],a[j]);
+//             }
+//             ++j;
+//         }
+//         ++i;
+//         swap(a[i],a[pivot]);
+//         quickSort(a,start,i-1);
+//         quickSort(a,i+1,end);
+//     }
+    
+//     public:
+//     int partition (int arr[], int start, int end)
+//     {
+       // Your code here
+//     }
+// };
