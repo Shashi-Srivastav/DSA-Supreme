02213279e7fec03ccd285ac6b989044015ff823a
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-11-06 22:36:35 +0530

    054

--- 000 LeetCode Problems/054 Beautiful Arrangement.cpp
+++ 000 LeetCode Problems/054 Beautiful Arrangement.cpp
@@ -1,27 +1,27 @@
-class Solution {
-public:
-    void anss(vector<int>&v,int&n,int&ans,int currN){
-        //base
-        if(currN == n + 1){
-            for(int i = 1;i<=n;i++){
-                cout<<v[i]<<" ";
-            }
-            cout<<endl;
-            ++ans;
-            return;
-        }
-        for(int i = 1;i<= n;i++){
-            if(v[i] == 0 && (currN % i == 0 || i % currN == 0)){
-                v[i] = currN;
-                anss(v,n,ans,currN+1);
-                v[i] = 0; // backt
-            }
-        }
-    }
-    int countArrangement(int n) {
-        vector<int>v(n+1);
-        int ans = 0;
-        anss(v,n,ans,1);
-        return ans;
-    }
-};
+// class Solution {
+// public:
+//     void anss(vector<int>&v,int&n,int&ans,int currN){
+//         //base
+//         if(currN == n + 1){
+//             for(int i = 1;i<=n;i++){
+//                 cout<<v[i]<<" ";
+//             }
+//             cout<<endl;
+//             ++ans;
+//             return;
+//         }
+//         for(int i = 1;i<= n;i++){
+//             if(v[i] == 0 && (currN % i == 0 || i % currN == 0)){
+//                 v[i] = currN;
+//                 anss(v,n,ans,currN+1);
+//                 v[i] = 0; // backt
+//             }
+//         }
+//     }
+//     int countArrangement(int n) {
+//         vector<int>v(n+1);
+//         int ans = 0;
+//         anss(v,n,ans,1);
+//         return ans;
+//     }
+// };
